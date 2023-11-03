40514b68e980109667956fc69f7ba517219047d8
Joan Llobera <joan.llobera@artanim.ch>
2023-11-03 16:06:00 +0100

    added quick and dirty calculation of velocities and angles, it needs to be debugged to make sure we are calculating the desired values

--- modular-agents/mujoco/Runtime/Components/MjFiniteDifferenceJoint.cs
+++ modular-agents/mujoco/Runtime/Components/MjFiniteDifferenceJoint.cs
@@ -28,6 +28,27 @@ public class MjFiniteDifferenceJoint : MonoBehaviour, IFiniteDifferenceComponent
         //I'm not sure we need to do anything here; it might be enough to step the body kinematics, then the joint components can remain largely stateless views into the body information.
     }
 
+    public void checkLocalAxisPos()
+    {
+        Quaternion q = MjEngineTool.MjQuaternion(gameObject.transform.rotation);
+
+        //Vector3 upMj = MjEngineTool.MjVector3Up;
+        Vector3 upMj = MjEngineTool.MjVector3(gameObject.transform.up);
+
+        Vector3 upUn = gameObject.transform.up;
+        //Vector3 test = LocalAngularVelocity;
+        Vector3 mjpos = MjEngineTool.MjVector3(gameObject.transform.position);
+        //return new double[3] { mjpos.x, mjpos.y, mjpos.z };
+
+        Vector3 ax = ((MjHingeJoint)PairedJoint).RotationAxis;
+
+
+            // Debug.Log(gameObject.name + " has up in unity: " + upUn + " up in mujoco: " + upMj + "  and local mj pos: " + mjpos + " rotation axis: " + ax);
+
+       IMjJointState imj= GetJointState();
+       Debug.Log(gameObject.name + " has vels: " + imj.Velocities + " has LOCAL ROTATIONS: " + imj.Positions);
+    }
+
     private abstract class FiniteDifferenceJointState
     {
         protected MjFiniteDifferenceJoint component;
@@ -84,10 +105,55 @@ public class MjFiniteDifferenceJoint : MonoBehaviour, IFiniteDifferenceComponent
 
         public double[] Accelerations => throw new System.NotImplementedException("Currently only first order joint states supported.");
 
-        public double[] Velocities => throw new System.NotImplementedException();  // TODO 
+        //  public double[] Velocities => throw new System.NotImplementedException();  // TODO 
 
-        public double[] Positions => throw new System.NotImplementedException();  // TODO
+        public double[] Velocities => getLocalAngularVelocity();
+
+
+        double[] getLocalAngularVelocity()
+        {
+           return  new double[3] { LocalAngularVelocity.x, 0, 0 };
+
+        }
+
+        double[] getLocalRotation()
+        {
+
+            Quaternion temp = new Quaternion(LocalRotation.x, 0, 0, 1).normalized;
+            return new double[3] { Mathf.Asin( 2* temp.x), 0, 0 };
+
+        }
+
+
+        //public double[] Positions => throw new System.NotImplementedException();   // TODO
+
+        public double[] Positions => getLocalRotation();
+
+
         //check into MjEngineTool.MjQuaternion for conversion
+
+      
+
+        //   double[] getLocalRotations()
+       public  void checkLocalRotations()
+        {
+            Quaternion q = MjEngineTool.MjQuaternion(gameObject.transform.rotation);
+
+            //Vector3 upMj = MjEngineTool.MjVector3Up;
+            Vector3 upMj = MjEngineTool.MjVector3(gameObject.transform.up);
+
+            Vector3 upUn = gameObject.transform.up;
+            Vector3 test = LocalAngularVelocity;
+            //Vector3 mjpos = MjEngineTool.MjVector3(gameObject.transform.position);
+            //return new double[3] { mjpos.x, mjpos.y, mjpos.z };
+
+            Debug.Log(gameObject.name + " has up in unity: " + upUn + " up in mujoco: " + upMj + "  and local angle velocity: " + test);
+
+        }
+
+
+
+
 
         public double[] PositionErrors => throw new System.NotImplementedException();  // TODO (Positions for hinge, quat error from identity for ball)
 
@@ -143,6 +209,18 @@ public class MjFiniteDifferenceJoint : MonoBehaviour, IFiniteDifferenceComponent
         }
     }
 
+
+    /*
+      // Transform
+      transform.localPosition =
+          MjEngineTool.UnityVector3(mjcf.GetVector3Attribute("pos", defaultValue: Vector3.zero));
+      transform.localRotation = MjEngineTool.UnityQuaternion(
+          mjcf.GetQuaternionAttribute("quat", defaultValue: MjEngineTool.MjQuaternionIdentity));
+      GravityCompensation = mjcf.GetFloatAttribute("gravcomp", defaultValue: 0.0f); 
+     
+     */
+
+
     private class FiniteDifferenceFreeJoint : FiniteDifferenceJointState, IMjJointState
     {
         MjFreeJoint freeJoint;
@@ -164,8 +242,17 @@ public class MjFiniteDifferenceJoint : MonoBehaviour, IFiniteDifferenceComponent
         public double[] Accelerations => throw new System.NotImplementedException();
 
         public double[] Velocities => throw new System.NotImplementedException();
+        /*
+        public override unsafe void OnSyncState(MujocoLib.mjData_* data)
+        {
+            transform.position = MjEngineTool.UnityVector3(
+                MjEngineTool.MjVector3AtEntry(data->xpos, MujocoId));
+            transform.rotation = MjEngineTool.UnityQuaternion(
+                MjEngineTool.MjQuaternionAtEntry(data->xquat, MujocoId));
+        }*/
 
         public double[] Positions => throw new System.NotImplementedException();
+        //public double[] Positions => MjEngineTool.MjVector3(transform) ;
 
         public double[] PositionErrors => throw new System.NotImplementedException();
 
