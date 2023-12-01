a7aff9d7b8cc61299fb77d57033495d433c8d1c9
Joan Llobera <joan.llobera@artanim.ch>
2023-12-01 17:45:46 +0100

    implemented the pause editor handler

--- modular-agents/core/Runtime/Training Event System/Debug Tools/PauseEditorHandler.cs
+++ modular-agents/core/Runtime/Training Event System/Debug Tools/PauseEditorHandler.cs
@@ -1,11 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PauseEditorHandler : TrainingEventHandler
 {
-    public override EventHandler Handler => throw new NotImplementedException();
+    // public override EventHandler Handler => throw new NotImplementedException();
+    public override EventHandler Handler => PauseInEditor;
 
+
+    void PauseInEditor(object sender, EventArgs eventArgs)
+    { 
+        EditorApplication.isPaused = true;
     
+    
+    }
+
+
 }
