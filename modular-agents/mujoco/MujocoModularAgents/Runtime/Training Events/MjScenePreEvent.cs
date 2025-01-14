d30d164a0d5fbec4b80b2d053a991acb4dcb1d10
Joan Llobera <joan.llobera@artanim.ch>
2025-01-14 11:31:01 +0100

    upgraded the mujoco version, checked straightwalk environments still work

--- modular-agents/mujoco/MujocoModularAgents/Runtime/Training Events/MjScenePreEvent.cs
+++ modular-agents/mujoco/MujocoModularAgents/Runtime/Training Events/MjScenePreEvent.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mujoco;
 
+
 public class MjScenePreEvent : TrainingEvent
 {
+
     public override void SubscribeHandler(EventHandler subscriber)
     {
-        MjScene.Instance.preUpdateEvent += subscriber;
+        MjScene.Instance.preUpdateEvent += (sender, args) => subscriber(sender, args);  // TODO: Confirm this works as intended
     }
 
     public override void UnsubscribeHandler(EventHandler subscribed)
     {
-        if (MjScene.InstanceExists) MjScene.Instance.preUpdateEvent -= subscribed;
+        if (MjScene.InstanceExists) MjScene.Instance.preUpdateEvent -= (sender, args) => subscribed(sender, args);
     }
 }
