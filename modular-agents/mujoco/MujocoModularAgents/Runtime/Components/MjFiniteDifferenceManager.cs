80042ef5b76b8ae1c090f3f1351e4823a1645ac1
Joan Llobera <joan.llobera@artanim.ch>
2025-07-07 14:17:15 +0200

    fixes to make FD training scenes work

--- modular-agents/mujoco/MujocoModularAgents/Runtime/Components/MjFiniteDifferenceManager.cs
+++ modular-agents/mujoco/MujocoModularAgents/Runtime/Components/MjFiniteDifferenceManager.cs
@@ -27,25 +27,61 @@ public class MjFiniteDifferenceManager :MonoBehaviour
   
     private void Start()
     {
-      managedComponents =  gameObject.transform.GetComponentsInChildren<MjFiniteDifferenceBody>();
-        
-        //to make sure they all have the right tracking of their parents we do:
-       foreach (MjFiniteDifferenceBody comp in managedComponents)
-            comp.GetIKinematic();
-       var fdJoints = GetComponentsInChildren<MjFiniteDifferenceJoint>();
-        orderedJoints = pairedRootJoint.GetComponentInParent<MjBody>().GetTopDownOrderedComponents<MjBaseJoint>().Select(j => fdJoints.First(fdj => fdj.PairedJoint == j)).ToList();
-        
+
+        MjState.ExecuteAfterMjStart(Initialize);
 
 
-        Debug.LogWarning("Set the option --timescale=1 when training a humanoid ragdoll from a reference based on Mujoco Finite Difference Bodies, \n" +
-                         " otherwise the method CopyStateToPairedTree(), used when resetting the humanoid, will not work well. ");
       
     }
 
 
+    private void Initialize()
+    {
+        
+        managedComponents = gameObject.transform.GetComponentsInChildren<MjFiniteDifferenceBody>();
+
+        //to make sure they all have the right tracking of their parents we do:
+        foreach (MjFiniteDifferenceBody comp in managedComponents)
+            comp.GetIKinematic();
+        var fdJoints = GetComponentsInChildren<MjFiniteDifferenceJoint>();
+
+
+        MjBody rootMjBody = pairedRootJoint.transform.parent.GetComponent<MjBody>();
+
+        
+
+        MjBaseJoint[] checkBaseJoints = rootMjBody.GetComponentsInChildren<MjBaseJoint>().ToArray();
+        checkBaseJoints = checkBaseJoints.OrderBy(x => x.MujocoId).ToArray();
+
+        orderedJoints = checkBaseJoints.Select(j => fdJoints.FirstOrDefault(fdj => fdj.PairedJoint == j)).ToList();
+
+        orderedJoints.RemoveAll(IsNull);
+
+
+        if (checkBaseJoints.Length != fdJoints.Length)
+            Debug.LogWarning($"I have {checkBaseJoints.Length}   checkBaseJoints and {fdJoints.Length}  FDjoints, some of the  {orderedJoints.Count} orderedJoints Will Not have anything");
+
+
+        Debug.LogWarning("Set the option --timescale=1 when training a humanoid ragdoll from a reference based on Mujoco Finite Difference Bodies, \n" +
+                         " otherwise the method CopyStateToPairedTree(), used when resetting the humanoid, will not work well. ");
+
+        CopyStateToPairedTree();
+
+
+    }
+
+    private static bool IsNull(MjFiniteDifferenceJoint fdj)
+    { 
+        return fdj==null;
+    
+    
+    }
+  
     public unsafe void CopyStateToPairedTree()
     {
-    
+
+       
+
         //MjState.TeleportMjRoot(pairedRootJoint, animationRoot.transform.position, animationRoot.transform.rotation);
 
         foreach (MjFiniteDifferenceJoint mfdj in orderedJoints)
@@ -56,7 +92,7 @@ public class MjFiniteDifferenceManager :MonoBehaviour
         ForwardKinematics();
 
     }
-
+    
 
 
     public void Step()
