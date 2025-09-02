bec466ff486a99985b34b30d4bc590a00fbb5918
Joan Llobera <joan.llobera@artanim.ch>
2025-09-02 14:57:04 +0200

    a first fix to the function setting up the finite body elements, which makes  the set up of the ragdoll work for the MjResetPose script (but not yet the puppet). This is taken from ht eFiniteDifferenceManagerEditor, hence the setup does not work (yet) in real time, it has to be prepared in editor

--- modular-agents/mujoco/MujocoModularAgents/Runtime/Components/MjResetPose.cs
+++ modular-agents/mujoco/MujocoModularAgents/Runtime/Components/MjResetPose.cs
@@ -39,9 +39,14 @@ public class MjResetPoseEditor : Editor
         if (GUILayout.Button("Generate Finite Difference Components"))
         {
                 MjResetPose t = target as MjResetPose;
-         
-            t.SetupFDElements(); ;
-        }
+
+                MjBody mjBody = t.mjRagdollRoot.GetComponentInParent<MjBody>();
+                //RecursiveComponentCreation(t, mjBody, t.transform, "");
+                RecursiveComponentCreation(t, mjBody,t.referenceRoot , "");
+
+
+                //t.SetupFDElements(); ;
+            }
 
         serializedObject.ApplyModifiedProperties();
 
@@ -49,11 +54,56 @@ public class MjResetPoseEditor : Editor
 
 
 
+        private void RecursiveComponentCreation(MjResetPose tar, MjBody mjBody, Transform parentTransform, string prefix)
+        {
+
+            List<Transform> childTransforms = new List<Transform>();
+            //this is supercustom for pupeteering case
+            //if (tar.useInPupeteering)
+            //         childTransforms = parentTransform.GetComponentsInChildren<Transform>().Where(t => t.name == "K_" + mjBody.name).Where(x => x.transform.GetComponent<MjBody>() != null).ToList();
+            // else
+
+            childTransforms = parentTransform.GetComponentsInChildren<Transform>().Where(t => t.name == prefix + mjBody.name).ToList();
+
+
+            if (childTransforms.Count() > 1)
+            {
+                Debug.LogWarning($"More than 1 match found for body {mjBody.name}: {string.Join(", ", childTransforms.Select(t => t.name))} Kinematic rig creation would likely fail.");
+                return;
+            }
+            if (childTransforms.Count() < 1)
+            {
+                Debug.LogWarning($"No match found for body {mjBody.name}. The corresponding animated transform is expected to share the name of the MjBody, being the ref:" + prefix + mjBody.name);
+                return;
+            }
+
+            MjFiniteDifferenceBody finiteDifferenceBody = childTransforms.First().gameObject.GetComponent<MjFiniteDifferenceBody>();
+            if (finiteDifferenceBody == null)
+            {
+                finiteDifferenceBody = childTransforms.First().gameObject.AddComponent<MjFiniteDifferenceBody>();
+
+            }
+
+
+            finiteDifferenceBody.PairedBody = mjBody;
+
+            foreach (var joint in mjBody.GetBodyChildComponents<MjBaseJoint>())
+            {
+                var finiteDifferenceJoint = new GameObject(prefix + joint.name).AddComponent<MjFiniteDifferenceJoint>();
+                finiteDifferenceJoint.transform.SetLocalPositionAndRotation(joint.transform.localPosition, joint.transform.localRotation);
+                finiteDifferenceJoint.transform.parent = finiteDifferenceBody.transform;
+                finiteDifferenceJoint.PairedJoint = joint;
+            }
+            foreach (var childBody in mjBody.GetBodyChildComponents<MjBody>())
+            {
+                RecursiveComponentCreation(tar, childBody, finiteDifferenceBody.transform, prefix);
+            }
+        }
 
 
 
-   
-}
+
+    }
 
 
 
@@ -86,7 +136,7 @@ public class MjResetPoseEditor : Editor
         public
         Avatar referenceAvatar;
 
-        [SerializeField]
+        public
         Transform referenceRoot;
 
         public
@@ -125,7 +175,7 @@ public class MjResetPoseEditor : Editor
             rootRagdoll = mjRagdollRoot.transform.parent.GetComponent<MjBody>();
             rootFDBody.PairedBody = rootRagdoll;
 
-            AddFDBodies(rootRagdoll, referenceRoot);
+            //AddFDBodies(rootRagdoll, referenceRoot);
 
 
             Initialize();
